ef61f9ba7e807b7844636f0739e4ad0cce4086ea
ohtaekwon <ohtk1119@gmail.com>
2022-07-16 23:47:14 +0900

    feat : 최근 개봉 섹션 작성

---
+++ src/features/movie/latest/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "@emotion/styled";
+import {} from "react-query";
+
+const LatestMovieSection: React.FC = () => {
+  return (
+    <Base>
+      <Title>최근 개봉작</Title>
+    </Base>
+  );
+};
+
+const Base = styled.div``;
+
+const Title = styled.h4``;
