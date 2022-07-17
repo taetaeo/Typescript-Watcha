f7f4727bb4e5dbbaceb93e1fef87cb0b076197f9
ohtaekwon <ohtk1119@gmail.com>
2022-07-17 20:22:26 +0900

    feat : 로딩 연결과, 최신영화 api연결

--- src/features/movie/latest/index.tsx
+++ src/features/movie/latest/index.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import styled from "@emotion/styled";
-import {} from "react-query";
+import useLatestMovie from "../../../hooks/useLatestMovie";
 
 const LatestMovieSection: React.FC = () => {
+  const { data, isLoading } = useLatestMovie();
+
   return (
     <Base>
       <Title>최근 개봉작</Title>
+      {isLoading ? <div>Loading....</div> : <div>{data?.data.title}</div>}
     </Base>
   );
 };
+
+export default LatestMovieSection;
 
 const Base = styled.div``;
 
