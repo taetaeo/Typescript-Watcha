96bd3f93d71dd4346e9744ad7749f4db361978cd
ohtaekwon <ohtk1119@gmail.com>
2022-07-17 23:25:32 +0900

    refactor : 버전 환경변수로 추가

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,12 +1,11 @@
-import axios from 'axios';
-
+import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: `${process.env.REACT_APP_API_HOST}/3`,
-  params : {
+  baseURL: `${process.env.REACT_APP_API_HOST}/${process.env.REACT_APP_API_VERSION}`,
+  params: {
     api_key: process.env.REACT_APP_API_KEY,
-    language: 'ko-KR'
-  }
-})
+    language: "ko-KR",
+  },
+});
 
-export default axiosInstance
+export default axiosInstance;
