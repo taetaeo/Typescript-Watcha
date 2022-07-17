4a8879a2edffe9f7fe19278cd2dfc0f6ab78113e
ohtaekwon <ohtk1119@gmail.com>
2022-07-17 20:20:22 +0900

    chore : 중복된 코드 제거와 client 연결

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryClient = new QueryClient();
 
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <QueryClientProvider>
+  <QueryClientProvider client={queryClient}>
     <React.StrictMode>
       <BrowserRouter>
         <App />
