4eb867776db3cd894e47f61e75ec92712a8e734d
ohtaekwon <ohtk1119@gmail.com>
2022-07-19 22:35:29 +0900

    feat : TV TYPE 정의

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -9,6 +9,52 @@ interface Language {
   iso_639_1: string;
   name: string;
 }
+
+interface Image {
+  aspect_ratio: number;
+  file_path: string;
+  height: number;
+  iso_639_1: null | string;
+  vote_average: number;
+  vote_count: number;
+  width: number;
+}
+
+export interface Dates {
+  maximum: string;
+  minimum: string;
+}
+
+export interface ListResponse<T> {
+  dates: Dates;
+  page: number;
+  results: T[];
+  total_pages: number;
+  total_results: number;
+}
+
+interface Person {
+  id: number;
+  name: string;
+  profile_path: string;
+  adult: boolean;
+  popularity: number;
+  known_for: Array<Movie | TV>;
+}
+interface Network {
+  id: number;
+  name: string;
+}
+
+interface Season {
+  id: number;
+  episode_count: number;
+  poster_path: string;
+  season_number: number;
+  air_date: Date;
+}
+
+// MOVIE TYPES
 export interface Movie {
   id: number;
   title: string;
@@ -40,31 +86,42 @@ export interface MovieDetail extends Movie {
   status: string;
   tagline: string;
 }
-interface Image {
-  aspect_ratio: number;
-  file_path: string;
-  height: number;
-  iso_639_1: null | string;
-  vote_average: number;
-  vote_count: number;
-  width: number;
-}
-
 export interface MovieImage {
   id: number;
   backdrop: Array<Image>;
   posters: Array<Image>;
 }
 
-export interface Dates {
-  maximum: string;
-  minimum: string;
+// TV TYPES
+interface Tv {
+  id: number;
+  name: string;
+  original_name: string;
+  poster_path: string;
+  popularity: number;
+  backdrop_path: string;
+  vote_average: number;
+  overview: string;
+  origin_country: string[];
+  genre_ids: number[];
+  original_lanuage: string;
+  vote_count: number;
+  first_air_date: string;
 }
 
-export interface ListResponse<T> {
-  dates: Dates;
-  page: number;
-  results: T[];
-  total_pages: number;
-  total_results: number;
+export interface TvDetail extends Tv {
+  created_by: Person[];
+  episode_run_time: number[];
+  genres: Genre[];
+  homepage: string;
+  in_production: boolean;
+  languages: string[];
+  networks: Network[];
+  number_of_episodes: number;
+  number_of_seasons: number;
+  production_companies: Company[];
+  seasons: Season[];
+  status: string;
+  type: string;
+  last_air_date: Date;
 }
