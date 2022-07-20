bf9ed36d32b651d5df14c6401cb9a1b676ca2d13
ohtaekwon <ohtk1119@gmail.com>
2022-07-21 00:10:29 +0900

    feat : airingToday 섹션 연결

--- src/pages/tv/index.tsx
+++ src/pages/tv/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import LatestMovieSection from "../../features/movie/latest";
+import AiringTodayTvSection from "../../features/tv/airingToday";
 
 const TvPage: React.FC = () => {
   return (
     <>
       <LatestMovieSection />
+      <AiringTodayTvSection />
     </>
   );
 };
